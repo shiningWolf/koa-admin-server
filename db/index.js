ffe74294397e3c41d70ae47609a1c9671d9c44f7
shiningWolf <403644529@qq.com>
2020-02-13 11:40:58 +0800

    增加连接数据库操作
    增加连接数据库操作

---
+++ db/index.js
@@ -0,0 +1,14 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/runoob";
+class DB{
+    init(){
+        MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+            if (err) throw err;
+            console.log("数据库已连接!");
+            this._db = db;
+        });
+    }
+}
+const db = new DB();
+module.exports = db;
