85be2c65cb28256f88f43762d1d13b99e923a08d
shiningWolf <403644529@qq.com>
2020-02-13 11:51:51 +0800

    this指向错误
    改为箭头函数

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/runoob";
 class DB{
     init(){
-        MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+        MongoClient.connect(url, { useNewUrlParser: true }, (err, db)=> {
             if (err) throw err;
             console.log("数据库已连接!");
             this._db = db;
