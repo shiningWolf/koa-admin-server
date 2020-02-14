30d97fe04ebdd752f63141626cfa1437e853a645
shiningWolf <403644529@qq.com>
2020-02-14 12:15:13 +0800

    完成分支管理的查，删除、添加
    完成分支管理的查，删除、添加

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const url = "mongodb://localhost:27017/runoob";
 class DB{
     init(){
@@ -20,6 +21,34 @@ class DB{
                     reolve(result)
                 }
             });
+        })
+    }
+
+    insert(tableName,data){ //插入数据
+        return new Promise((resolve,reject)=>{
+            let dbo = this._db.db("runoob");
+            dbo.collection(tableName).insertOne(data,(err,res)=>{
+                if (err) {
+                    reject()
+                }else {
+                    resolve()
+                }
+            })
+        })
+    }
+
+    update(tableName,id,data){ //更新数据
+        return new Promise((resolve,reject)=>{
+            let dbo = this._db.db("runoob");
+            let updateStr = {$set: data};
+            dbo.collection(tableName).updateOne({_id:ObjectID(id)},updateStr,(err,res)=>{
+                if (err) {
+                    console.log('update',err)
+                    reject()
+                }else {
+                    resolve()
+                }
+            })
         })
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,75 @@ const router = require('koa-router')()
 const db = require('../db')
 router.prefix('/dev-api/vue-admin-template')
 
+router.get('/user/login', async function (ctx, next) {
+ ctx.body = {
+   code:20000,
+   data:{token: "admin-token"}
+ }
+})
+
+router.get('/user/info', async function (ctx, next) {
+  ctx.body = {
+    code:20000,
+    data: {
+      "roles": [
+        "admin"
+      ],
+      "introduction": "I am a super administrator",
+      "avatar": "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
+      "name": "Super Admin"
+    }
+  }
+})
+
 router.get('/branchmanage/list', async function (ctx, next) {
   try {
     let result = await db.search('branches');
+    ctx.body = {
+      code:20000,
+      data:result
+    }
+  } catch(e) {
+    ctx.body = {
+      code:500
+    }
+  }
+})
+
+router.post('/branchmanage/create', async function (ctx, next) {
+  let body = ctx.request.body;
+  console.log('body',typeof body,body)
+  try {
+    let result = await db.insert('branches',body);
+    ctx.body = {
+      code:20000
+    }
+  } catch(e) {
+    ctx.body = {
+      code:500
+    }
+  }
+})
+
+router.post('/branchmanage/update', async function (ctx, next) {
+  let body = ctx.request.body;
+  let id = body._id;
+  delete body._id;
+  try {
+    let result = await db.update('branches',id,body);
+    ctx.body = {
+      code:20000
+    }
+  } catch(e) {
+    ctx.body = {
+      code:500
+    }
+  }
+})
+
+router.get('/problem/list', async function (ctx, next) {
+  try {
+    let result = await db.search('problem');
     ctx.body = {
       code:20000,
       data:result
